3a1cc4616b47cb8dfc173838be55e3da663563b9
Chien-Te Lee <Dexter.ChienTeLee@gmail.com>
2018-11-19 23:15:41 -0800

    Add doxygen comment - add doxygen comment for source code

--- include/walker.h
+++ include/walker.h
@@ -1,16 +1,81 @@
+/**
+ * BSD 3 clauses Liscense
+ *
+ * Copyright <2018> <Chien-Te Lee>
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions 
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in 
+ * the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from 
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** @file walker.h
+ *  @brief definition of walker class
+ *  @copyright (c) 2018 Chien-Te Lee
+ *  @author Chien-Te Lee
+ *  @date   11/20/2018
+ *
+ *  This program contains definition of walker class.
+ *  
+ */
+
+
+#ifndef INCLUDE_WALKER_H_
+#define INCLUDE_WALKER_H_
+
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Twist.h>
 
-
+/**
+ *  @brief definition of walker class
+ *  @param none
+ *  @return none
+ */
 class walker{
 public:
+    /**
+     *  @brief constructor of walker class
+     *  @param none
+     *  @return none
+     */
     walker();
 
 private:
+    ///< geometry twist message of velocity to be pulished
     geometry_msgs::Twist msg;
+
+    ///< handle ros node
     ros::NodeHandle n;
+
+    ///< ros publisher to publish velocity
     ros::Publisher veloclity_pub;
+
+    ///< ros subscriber to read laserscan data
     ros::Subscriber laserScan_sub;
+
+    /**
+     *  @brief This function checks wheter the robot needs to turn
+     *  @param scan is the received laserscan data
+     *  @return none
+     */
     void laserScanCallBack(const sensor_msgs::LaserScan::ConstPtr& scan);
 };
+
+
+#endif    // INCLUDE_WALKER_H_
+
+
--- src/walker.cpp
+++ src/walker.cpp
@@ -1,29 +1,75 @@
+/**
+ * BSD 3 clauses Liscense
+ *
+ * Copyright <2018> <Chien-Te Lee>
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions 
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in 
+ * the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from 
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#include "walker.h"
+/** @file walker.cpp
+ *  @brief implementation of walker class
+ *  @copyright (c) 2018 Chien-Te Lee
+ *  @author Chien-Te Lee
+ *  @date   11/20/2018
+ *
+ *  This program implements fucntions of walker class.
+ *  
+ */
 
-walker::walker()
-{
+#include <walker.h>
+
+/**
+ *  @brief constructor of walker class
+ *  @param none
+ *  @return none
+ */
+walker::walker() {
     msg.linear.x = 0.0;
     msg.linear.y = 0.0;
     msg.linear.z = 0.0;
     msg.angular.x = 0.0;
     msg.angular.y = 0.0;
     msg.angular.z = 0.0;
-    veloclity_pub = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 10);
-    laserScan_sub = n.subscribe<sensor_msgs::LaserScan>("/scan",10, &walker::laserScanCallBack, this); 
+    veloclity_pub = n.advertise<geometry_msgs::Twist>
+                      ("/mobile_base/commands/velocity", 10);
+    laserScan_sub = n.subscribe<sensor_msgs::LaserScan>
+                      ("/scan", 10, &walker::laserScanCallBack, this);
 }
 
 
-void walker::laserScanCallBack(const sensor_msgs::LaserScan::ConstPtr& scan){
+/**
+ *  @brief This function checks wheter the robot needs to turn based on laserscan data
+ *  @param scan is the received laserscan data
+ *  @return none
+ */
+void walker::laserScanCallBack(const sensor_msgs::LaserScan::ConstPtr& scan) {
     int ranges = scan->ranges.size();
     bool turn = false;
 
-    for(int i = 0; i < ranges; ++i){
+    // checks wheter the distance to wall is less than 0.8
+    for (int i = 0; i < ranges; ++i) {
         if (scan->ranges[i] < 0.8)
             turn = true;
     }
 
-    if(turn){
+    // if too close, start turning, else go straight
+    if (turn) {
         msg.linear.x = 0.0;
         msg.angular.z = 0.5;
     } else {
@@ -31,7 +77,7 @@ void walker::laserScanCallBack(const sensor_msgs::LaserScan::ConstPtr& scan){
         msg.angular.z = 0.0;
     }
 
+    // publish the velocity immediately
     veloclity_pub.publish(msg);
-
 }
 
